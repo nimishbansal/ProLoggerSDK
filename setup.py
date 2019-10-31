9397b3684714750af373f597df85b63b8357ce48
nimish <nimish4july1998@gmail.com>
2019-10-31 19:19:51 +0530

    modified setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="prologgersdk-pkg-nimishbansal",
+    version="0.0.1",
+    author="Nimish Bansal",
+    author_email="nimish4july1998@gmail.com",
+    description="your personal logger for your projects",
+    long_description="your personal logger for your projects",
+    long_description_content_type="text/markdown",
+    url="https://github.com/nimishbansal/ProLoggerSDK",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
